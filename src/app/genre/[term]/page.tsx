bd9f4f59a7b2da055c0588d5aedff2528b22577d
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-05 10:30:42 +0000

    Ensure the dynamic pages for the genre are meet

--- src/app/genre/[term]/page.tsx
+++ src/app/genre/[term]/page.tsx
@@ -1,3 +1,5 @@
+import MovieCarousel from "@/components/MovieCarousel";
+import { getDiscoverMovies } from "@/lib/getMovies";
 
 type Props = {
     params: {
@@ -8,10 +10,15 @@ type Props = {
     }
 }
 
-function page({ params: { term}, searchParams: {genre} }: Props) {
-
+async function page({ params: { term}, searchParams: {genre} }: Props) {
+  const movies = await getDiscoverMovies(term)
   return (
-    <div>This is the genre {term} and parmas: {genre} </div>
+    <div className="max-w-7xl mx-auto">
+       <div className="flex flex-col space-y-5 mt-32 xl:mt-32 ">
+        <h1>Results for {genre}</h1>
+        <MovieCarousel title="Genre" movies={movies} isVertical/>
+       </div>
+       </div>
   )
 }
 
