b7e39f04d49670582997c3e2239a00147e597d2b
Baah-Danso-Kenneth <kwadwobaahdanso@gmail.com>
2025-01-05 10:31:12 +0000

    Search dynamic pages create to sync well

--- src/app/search/[id]/page.tsx
+++ src/app/search/[id]/page.tsx
@@ -1,16 +1,25 @@
+import MovieCarousel from '@/components/MovieCarousel'
+import { getPopularMovies, getSearchMovies } from '@/lib/getMovies'
 import React from 'react'
 
 type Props={
     params:{
         id:string
-    },
-    searchParams:{
-        search:string
     }
 }
-function page({ params: { id}, searchParams: {search} }: Props) {
+async function page({ params: { id}}: Props) {
+  const decode = decodeURI(id)
+
+  const movies = await getSearchMovies(decode)
+  const popularMovies = await getPopularMovies()
   return (
-    <div>Search id is : {id} item search: {search}</div>
+    <div className='max-w-7xl mx-auto'>
+         <div className='flex flex-col space-y-4 mt-32 '>
+          <h1 className='text-6xl font-bold'>Results for {decode}</h1>
+          <MovieCarousel title='Movies' movies={movies} isVertical/>
+          <MovieCarousel title='You may also like' movies={popularMovies}/>
+         </div>
+      </div>
   )
 }
 
